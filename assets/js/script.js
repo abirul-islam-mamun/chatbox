5e8cb0288934a74092956a46780f4570e9782c8e
abirul-islam-mamun <mamun.billa444@gmail.com>
2023-02-20 13:08:50 +0600

    chatbox

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+//menu right (heart) Active 
+
+$('.details-option').on('click',function(){
+    $(this).toggleClass('active')
+})
+
+$('.details-option').on('click',function(){
+    $(this).toggleClass('detials-show')
+})
+
+
+
+
+// $('.details-option').on('click',function(){
+//     $('.details-option').removeClass('active');
+//     $(this).addClass('active')
+// })
